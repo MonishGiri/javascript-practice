6975e88e0ab1a5c1bfff48f189021f3acf45d2b1
MonishGiri <monishgiri46@gmail.com>
2024-08-07 14:39:07 +0530

    Conversion of datatypes

---
+++ Basics/3_ConversionOperation.js
@@ -0,0 +1,28 @@
+let score = "50abc";
+console.log( typeof score);
+
+let valueInNumber = Number(score);
+// console.log( typeof valueInNumber); 
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc => NaN"
+// true => 1
+// false => 0
+
+let isLoggedIn = "Monish";
+
+let booleanisLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanisLoggedIn);
+
+// 1 => true;
+// 0 => false
+// "" => false
+// "Monish" => Monish
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
