9b841e7d90db9395847f667fba063f269c3d7736
MonishGiri <monishgiri46@gmail.com>
2024-08-07 14:26:37 +0530

    Datatypes in js

---
+++ Datatypes/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+4); we are using node js not browser
+
+let name = "Monish";
+let age = 22;
+let isLoggedIn = true;
+
+
+// number => 2^53
+// bigint 
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined
+// Symbol => unique
+
+// object
+
+// null is of object type
+console.log(typeof null);
+console.log(typeof undefined); // object
