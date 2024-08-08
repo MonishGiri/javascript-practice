7537bc07e6b9bf114779f89f838aa05525586c17
MonishGiri <monishgiri46@gmail.com>
2024-08-08 08:15:01 +0530

    Strings in js

---
+++ Basics/6_Strings.js
@@ -0,0 +1,35 @@
+const name = "monish";
+const repoCount = 50;
+
+// console.log(name + repoCount + "value");
+// console.log(`Hello My name is ${name} my repo count is ${repoCount} value`);
+
+const gameName = new String("Monish-Giri-com");
+
+// console.log(typeof gameName);
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('i'));
+
+const newString = gameName.substring(0,4);
+// console.log(newString);
+
+const anotherString = gameName.slice(-8,4); // also take negative value
+// console.log(anotherString);
+
+const newStringOne = "        Monish            ";
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://monish.com%40Giri";
+// console.log(url.replace('%40','-'));
+// console.log(url.includes("monish"));
+// console.log(url.includes("giri"));
+
+console.log(gameName.split('-'));
+
