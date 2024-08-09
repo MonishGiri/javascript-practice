690034ddc6dc082893333bdf83d6d1ccb572f871
MonishGiri <monishgiri46@gmail.com>
2024-08-09 20:13:06 +0530

    Passing Objects and Arrays to functions

--- Functions/01_functions.js
+++ Functions/01_functions.js
@@ -33,4 +33,37 @@ function loginUserMsg(username = "sam"){
 }
 
 // console.log(loginUserMsg("Monish"));
-console.log(loginUserMsg()); // it will return undefined
+// console.log(loginUserMsg()); // it will return undefined
+
+// function calculateCartPrice(...num1){ // passed multiple value to the function using rest operator
+//     return num1;
+// }
+function calculateCartPrice(val1, val2,...num1){ // will return 600 and 700 first two values is taken by val1 and val2
+    return num1;
+}
+
+// console.log(calculateCartPrice(200,500,600,700));
+
+const user = {
+    username: "monish",
+    price: 199
+}
+
+function handleObj(anyObj){
+    console.log(`Username is ${anyObj.username} and price is ${anyObj.price}`)
+}
+
+// handleObj(user);
+
+handleObj({
+    username: "sahil",
+    price: 852
+})
+
+const myNewArr = [2,8,6,2];
+
+function returnSecondVal(arr){
+    return arr[1];
+}
+// console.log(returnSecondVal(myNewArr));
+console.log(returnSecondVal([5,6,3,5]));
